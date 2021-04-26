f1a340f5354d23f772bc63a799b8cfecebdcb8af
LCagmac <42069097+LCagmac@users.noreply.github.com>
2021-04-26 13:23:03 -0400

    Update wavetablesynth.py

--- wavetablesynth.py
+++ wavetablesynth.py
@@ -1,13 +1,16 @@
 import numpy as np
+from scipy.io.wavfile import read as audioread
+
+#t = np.linspace(0, 1, num=100)
+#wavetable = np.sin(np.sin(2 * np.pi * t)) # sine wave
+#wavetable1 = t * (t < 0.5) + (-(t - 1)) * (t>= 0.5) #triangle wave
 
 
-t = np.linspace(0, 1, num=100)
-wavetable = np.sin(np.sin(2 * np.pi * t)) # sine wave
-wavetable1 = t * (t < 0.5) + (-(t - 1)) * (t>= 0.5) #triangle wave
-
-
-def synthesize(sampling_speed, wavetable, n_samples):
+def synthesize(sampling_speed, n_samples, cAudioFilePath):
     """Synthesizes a new waveform from an existing wavetable."""
+    fs, x = audioread(cAudioFilePath)
+    t = np.linspace(0, 1, num=100)
+    wavetable = np.sin(np.sin(2 * np.pi * t)) # sine wave
     samples = []
     current_sample = 0
     while len(samples) < n_samples:
@@ -17,17 +20,21 @@ def synthesize(sampling_speed, wavetable, n_samples):
         current_sample += 1
     return np.array(samples)
 
-def synthesize1(sampling_speed, wavetable1, n_samples):
+def synthesize1(sampling_speed, n_samples, cAudioFilePath):
     """Synthesizes a new waveform from an existing wavetable."""
+    fs, x = audioread(cAudioFilePath)
+    t = np.linspace(0, 1, num=100)
+    wavetable1 = t * (t < 0.5) + (-(t - 1)) * (t>= 0.5) #triangle wave
     samples = []
     current_sample = 0
     while len(samples) < n_samples:
         current_sample += sampling_speed
-        current_sample = current_sample % wavetable.size
-        samples.append(wavetable[current_sample])
+        current_sample = current_sample % wavetable1.size
+        samples.append(wavetable1[current_sample])
         current_sample += 1
     return np.array(samples)
 
 
-sample1 = synthesize(440, wavetable, 2*fs)
-sample2 = synthesize1(440, wavetable1, 2 *fs)
+#sample1 = synthesize(440, 300, "ProjectstudiomvmtIIfinal.wav")
+#sample2 = synthesize1(440, 800,"ProjectstudiomvmtIIfinal.wav")
+
