7bad515c1fdbc92a870d7a0b73619efb0cd277dc
LCagmac <42069097+LCagmac@users.noreply.github.com>
2021-04-26 13:12:47 -0400

    Update resampling1.py

--- resampling1.py
+++ resampling1.py
@@ -1,15 +1,24 @@
 import numpy as np
 from scipy import interpolate
+from scipy.io.wavfile import read as audioread
+from scipy.io.wavfile import write as audiowrite
 
-new_rate = 22050
-fs, x = wavfile.read("")
 
-if fs != new_rate:
-    duration = x.shape[0] / fs
 
-    time_old = np.linspace(0, duration, x.shape[0])
-    time_new = np.linspace(0, duration, int(x.shape[0] * new_rate/fs))
-    interpolator = interpolate.interp1d(time_old, x.T)
-    new_audio = interpolator(time_new).T
+def resampling(new_rate, cAudioFilePath):
+    fs, x = audioread(cAudioFilePath)
+    if fs != new_rate:
+                duration = x.shape[0] / fs
+                time_old = np.linspace(0, duration, x.shape[0])
+                time_new = np.linspace(0, duration, int(x.shape[0] * new_rate/fs))
+                interpolator = interpolate.interp1d(time_old, x.T)
+                new_audio = interpolator(time_new).T
+        
+    audiowrite("out1.wav", new_rate, np.round(new_audio).astype(x.dtype))
+    
+#new_rate = 22050
+#fs, x = wavfile.read("")
+pizza = resampling(22050, "ProjectstudiomvmtIIfinal.wav")    
 
-    wavfile.write("out1.wav", new_rate, np.round(new_audio).astype(x.dtype))
+
+
